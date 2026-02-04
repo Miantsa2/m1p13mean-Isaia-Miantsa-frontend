ec2a0721fa9ff025508408ea59c01df7ea6e9428
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-04 18:44:37 +0300

    [ADD] Authentifications services to call api

---
+++ frontend/services/auth.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:5000/mean/auth'; 
+
+  constructor(private http: HttpClient) { }
+
+  signup(user: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signup`, user);
+  }
+
+  // login(email: string, password: string): Observable<any> {
+  //   return this.http.post(`${this.apiUrl}/login`, { email, password });
+  // }
+
+  login(email: string, password: string) {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { email, password })
+      .pipe(tap(res => localStorage.setItem('token', res.token)));
+  }
+
+  loginWithGoogle(): void {
+    window.location.href = `${this.apiUrl}/google`;
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  // getHome() {
+  //   this.http.get('http://localhost:5000/mean/client/home').subscribe(console.log);
+  // }
+
+  //  getHome2() {
+  //   this.http.get('http://localhost:5000/mean/client/test').subscribe(console.log);
+  // }
+}
