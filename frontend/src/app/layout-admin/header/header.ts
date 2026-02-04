6d576b376fd4868155f1a55f1b25be5f1d226902
Isaia_Joëlle <977jvmmwbt@privaterelay.appleid.com>
2026-02-04 17:41:57 +0300

    Modification récente des components y compris les templates
    Une autre modification

---
+++ frontend/src/app/layout-admin/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  standalone: true,
+  templateUrl: './header.html',
+})
+export class Header {
+
+}
