a78ba29e23b2611006db2ced60ea17eb5f2c5de2
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-04 18:49:32 +0300

    [IMPL] Signup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { MainLayout } from './layout_boutique/main-layout/main-layout';
 import { LoginComponent } from './pages/login/login';
+import { SignComponent } from './pages/sign/sign';
 import { MainLayoutAdmin } from './layout-admin/main-layout/main-layout';
 
 export const routes: Routes = 
@@ -24,7 +25,7 @@ export const routes: Routes =
   },
   {
     path: 'sign',
-    component: Sign
+    component: SignComponent
   }
 
 ];
--- frontend/src/app/pages/sign/sign.ts
+++ frontend/src/app/pages/sign/sign.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+
 
 @Component({
   selector: 'app-sign',
@@ -9,6 +12,48 @@ import { FormsModule } from '@angular/forms';
   imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './sign.html',
 })
-export class Sign {
+
+export class SignComponent {
+  newClient = {
+    email: '',
+    password: '',
+    nom: '',
+    prenom: '',
+    genre: '',
+    dateNaissance: '',
+    adresse: ''
+  };
+
+  constructor(private authService: AuthService,private router: Router) {}
+
+  onSignup(form: NgForm) {
+    this.authService.signup(this.newClient).subscribe({
+      next: (res) => {
+        this.authService.setToken(res.token);
+        console.log('Inscription réussie', res.user);
+        // this.router.navigate(['/dashboard']);
+      },
+       error: (err) => {
+       form.controls['sign']?.setErrors({
+        invalidCredentials: err.error?.message || 'Something went wrong during signup'
+      });
+    }
+    });
+  }
+
+  onGoogleSignup() {
+    this.authService.loginWithGoogle();
+  }
+
+
+
+  //  test() {
+  //   this.authService.getHome();
+  // }
+
+  //  test2() {
+  //   this.authService.getHome2();
+  // }
+
 
 }
