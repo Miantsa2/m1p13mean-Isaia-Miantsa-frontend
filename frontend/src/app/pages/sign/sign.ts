ccbe0e7f5e7c3c4b99937d6f6c6306a8c7976ac8
Isaia_Joëlle <977jvmmwbt@privaterelay.appleid.com>
2026-02-04 17:45:30 +0300

    Léger truc manquant pour le sign

--- frontend/src/app/pages/sign/sign.ts
+++ frontend/src/app/pages/sign/sign.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './sign.html',
 })
 export class Sign {
