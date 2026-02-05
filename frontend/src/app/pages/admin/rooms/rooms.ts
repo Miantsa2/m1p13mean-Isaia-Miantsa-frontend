529d9d6a84c1561924d9559f8bef4d22b50b04fb
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-05 17:04:21 +0300

    [UPD] room.ts

--- frontend/src/app/pages/admin/rooms/rooms.ts
+++ frontend/src/app/pages/admin/rooms/rooms.ts
@@ -1,33 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableCorps, TableColumn } from '../../../components/table-corps/table-corps';
 import { ButtonPrimaire } from '../../../components/button-primaire/button-primaire';
 import { ModalForm } from '../../../components/modal-form/modal-form';
+import { SalleService } from '../../../services/salle';
+
+
+
 @Component({
   selector: 'app-rooms',
   standalone: true,
   imports: [CommonModule, TableCorps, ButtonPrimaire, ModalForm],
   templateUrl: './rooms.html',
 })
-export class Rooms {
+export class Rooms implements OnInit {
+
   roomColumns: TableColumn[] = [
     { key: 'reference', label: 'Room Reference' },
-    { key: 'size', label: 'Size in m²' },
-    { key: 'date', label: 'Completion date' },
-    { key: 'status', label: 'Status' },
+    { key: 'tailleMetreCarre', label: 'Size in m²' },
+    { key: 'createdAt', label: 'Completion date' },
+    { key: 'statut', label: 'Status' },
     { key: 'actions', label: 'Actions' }
   ];
 
-  // Données de test
-  rooms = [
-    { ref: 'RM001', size: 10, date: '01-12-2025', status: 'Occupied' },
-    { ref: 'RM002', size: 25, date: '15-01-2026', status: 'Occupied' },
-    { ref: 'RM003', size: 15, date: '14-02-2026', status: 'Free' },
-  ];
+  rooms: any[] = []; 
+  meterPrice: number = 0;
 
   // Modal section
   isModalOpen = false;
   isPriceModalOpen = false;
+
+  constructor(private salleService: SalleService) {}
+
+  ngOnInit(): void {
+    this.loadRooms();
+  }
+
+  loadRooms(): void {
+    this.salleService.getRoom().subscribe({
+      next: (res) => {
+        this.rooms = res.map((room: any) => ({
+          _id: room._id,
+          reference: room._id,
+          size: room.tailleMetreCarre,
+          date: new Date(room.createdAt).toLocaleDateString(),
+          status: room.statut === 'libre' ? 'Free' : 'Occupied'
+        }));
+        console.log(this.rooms);
+
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
 
   openCreateModal() { this.isModalOpen = true; }
   closeCreateModal() { this.isModalOpen = false; }
