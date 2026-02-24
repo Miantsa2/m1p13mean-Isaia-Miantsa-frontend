e89662f4d9a30ff4e8082459050076dd037a18d4
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-24 15:59:25 +0300

    [FIX]url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = { 
   production: true, 
   // apiUrl: 'http://localhost:5000',
-  apiUrl: 'https://m1p13mean-isaia-miantsa-backend-production.onrender.com',
+  apiUrl: 'https://m1p13mean-isaia-miantsa-backend.onrender.com',
   STRIPE_PUBLIC_KEY: 'pk_test_51SzdBpDG6YpV6l17MebfJ31oXYQ91LLwtwzw9kwrZUdPGaU4I7RlEQOoFKRYqmq1Z0eArwp0raEPhtS2QGVpp5Oe00Anq1B26U'
 
 }; 
