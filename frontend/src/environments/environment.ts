47cd157806bfbe36a0dee638ac7448fb6f096079
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-25 19:27:50 +0300

    [UPD] apiUrl

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = { 
   production: true, 
-   apiUrl: 'http://localhost:5000',
-  //apiUrl: 'https://m1p13mean-isaia-miantsa-backend.onrender.com',
+  // apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://m1p13mean-isaia-miantsa-backend.onrender.com',
   STRIPE_PUBLIC_KEY: 'pk_test_51SzdBpDG6YpV6l17MebfJ31oXYQ91LLwtwzw9kwrZUdPGaU4I7RlEQOoFKRYqmq1Z0eArwp0raEPhtS2QGVpp5Oe00Anq1B26U'
 
 }; 
