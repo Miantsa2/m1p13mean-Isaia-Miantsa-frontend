628aff79ee1c5cd25c099dafaec93f48007331d3
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-25 18:38:32 +0300

    [FIX] reset forms rooms

--- frontend/src/app/pages/admin/event/event.ts
+++ frontend/src/app/pages/admin/event/event.ts
@@ -129,6 +129,11 @@ export class Events implements OnInit {
     });
   }
 
+  formatDateForInputUTC(dateInput: string | Date): string {
+  const d = new Date(dateInput);
+  const pad = (n: number) => n.toString().padStart(2, '0');
+  return `${d.getUTCFullYear()}-${pad(d.getUTCMonth()+1)}-${pad(d.getUTCDate())}T${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}`;
+}
 
   
   updateEvent() {
@@ -208,8 +213,8 @@ export class Events implements OnInit {
       type: event.type,
       statut: event.statut,
       description: event.description,
-      dateDebut: this.formatDateForInput(event.dateDebut),
-      dateFin: this.formatDateForInput(event.dateFin)
+      dateDebut:  this.formatDateForInputUTC(event.dateDebut),
+      dateFin:  this.formatDateForInputUTC(event.dateFin)
     };
     this.currentEditingId= event._id
     this.isEditModalOpen = true; 
