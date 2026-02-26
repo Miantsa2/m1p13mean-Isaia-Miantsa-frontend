3229266bf3b53643939a7696c805c5502f99d7e1
Miantsa2 <ranaivosonmiantsa@gmail.com>
2026-02-26 12:01:10 +0300

    reset all config

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -156,7 +156,7 @@ export const routes: Routes =
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
